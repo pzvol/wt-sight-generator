2c05c3ef6120e4e902c84770374b2156dc849d61
pzvol <z12321a@hotmail.com>
2023-04-22 06:28:49 +0800

    Added until circle classes

--- sight_lib.js
+++ sight_lib.js
@@ -2,24 +2,22 @@
 
 
 export default {
-	SIGHT_LIB_VER: "0.0.1",
+	SIGHT_LIB_VER: "0.0.2",
 }
 
 
 
+
+//// GENERALS ////
+
 /** A sight (text) file */
 export class Sight {
 	/** Available block names in a sight file */
-	static blockTitle = {
-		vehicleClass: "matchExpClass",
-		crosshairHorRanges: "crosshair_hor_ranges",
-		shellDistances: "crosshair_distances",
-		circles: "drawCircles",
+	static blockName = {
 		quads: "drawQuads",
 		lines: "drawLines",
 		texts: "drawTexts",
 	}
-
 
 	constructor() {
 		this.text = "";
@@ -46,11 +44,36 @@ export class Sight {
 
 
 /**
+ * Tools for writing
+ */
+export class Toolbox {
+	/** Get a number range array */
+	static range(start, end, step = 1.0) {
+		let result = [];
+		for (let i = start; i < end; i += step) { result.push(i); }
+		return result;
+	}
+	/** Get a number range array including the ending number */
+	static rangeIE(start, end, step = 1.0) {
+		let result = [];
+		for (let i = start; i <= end; i += step) { result.push(i); }
+		return result;
+	}
+
+	/** Clacuate thousandth value with given data (in meter) */
+	static calcThousandth(tgtWidth, distance) {
+		return ((1000 * tgtWidth) / distance)
+	}
+}
+
+
+/**
  * Sight general setting lines
  */
 export class General {
 	/**
-	 * Generate a new line of variable info
+	 * Generates a new line of compiled variable info
+	 * (without ending newline by default)
 	 *
 	 * @param {string} vName variable name
 	 * @param {number|string|boolean|number[]} vValue
@@ -90,28 +113,60 @@ export class General {
 		return `${vName}:${vTypeOut} = ${vValueOut}${end}`
 	}
 
-	static block(bName, bLines=[], inBlockLineEnd="\n", inBlockLineIndent="\t") {
-		let result = `${bName} { ` + inBlockLineEnd;
-		for (let l of bLines) {
-			// Indent every line
-			result +=
-				l.replace(new RegExp(`^`, "gm"), `${inBlockLineIndent}`) +
-				inBlockLineEnd;
+	/** Generates code of compiled general block inBlockLineEnd="\n", inBlockLineIndent="\t"*/
+	static block(bName, bLines=[], {
+		useOneLine = false,
+		baseIndentLevel = 0,
+		lineIndent = "\t",
+		oneLineSeparator = "; ",
+		multiLineEnd = "\n",
+	}={}) {
+		// Empty block:
+		if (bLines.length === 0) {
+			return `${bName} {}`;
 		}
-		result += "}";
 
+		let baseIndent = (() => {
+			let s = "";
+			for (let i = 0; i < baseIndentLevel; i++) { s += lineIndent; }
+			return s;
+		})();
+
+		// One line mode:
+		if (useOneLine) {
+			let result = baseIndent + `${bName} { `;
+			for (let l of bLines) {
+				result += l + oneLineSeparator;
+			}
+			result += "}";
+			return result;
+		}
+
+		// Multiple lines mode:
+		let inBlockIndentChars = baseIndent + lineIndent;
+		let lineStartRegExp = (new RegExp("^", "gm"));
+		let result = baseIndent + `${bName} { ` + multiLineEnd;
+		for (let l of bLines) {
+			result +=
+				l.replace(lineStartRegExp, inBlockIndentChars) +
+				multiLineEnd;
+		}
+		result += baseIndent + "}";
 		return result;
 	}
 }
 
 
 
-export class MatchVehicleClass {
-	static vehicleGroundDefaults = [
+
+//// BLOCKS ////
+
+/** Matched vehicle types (originall named as `matchExpClass`) */
+export class MatchVehicleClassBlock {
+	static vehicleTypeGroundDefaults = [
 		"exp_tank", "exp_heavy_tank", "exp_tank_destroyer"
 	];
-	static vehicleSPAAs = ["exp_SPAA"];
-
+	static vehicleTypeSPAAs = ["exp_SPAA"];
 
 	/**
 	 * Generate a "matchExpClass" block text
@@ -119,7 +174,7 @@ export class MatchVehicleClass {
 	 * @param {string[]} includeCls
 	 * @param {string[]} excludeCls
 	 */
-	static buildBlock(includeCls = [], excludeCls = [], inBlockLineEnd="\n", inBlockLineIndent="\t") {
+	static buildBlock(includeCls = [], excludeCls = []) {
 		let innerLines = [];
 		for (let c of includeCls) {
 			innerLines.push(General.variable(c, true));
@@ -128,37 +183,138 @@ export class MatchVehicleClass {
 			innerLines.push(General.variable(c, false));
 		}
 
-		return General.block(
-			Sight.blockTitle.vehicleClass, innerLines,
-			inBlockLineEnd, inBlockLineIndent
-		)
+		return General.block("matchExpClass", innerLines)
 	}
 }
 
 
+/**
+ * Thousandth lines on the sight horizon for measuring enemy distance
+ * (originall named as `crosshair_hor_ranges`)
+ */
+export class HorizontalThousandthsBlock {
+	constructor() { this.thousandthLines = []; }
+
+	/**
+	 * Adds a new line
+	 *
+	 * @param {number} thousandth line thousandth value
+	 * @param {number} shown displayed number value, `0` to hide the number
+	 */
+	add(thousandth, shown = 0) {
+		this.thousandthLines.push([thousandth, shown])
+	}
+
+	getCode() {
+		this.thousandthLines.sort((a, b) => (a[0] - b[0]))
+
+		let compiledLines = []
+		for (let lineInfo of this.thousandthLines) {
+			compiledLines.push(General.variable("range", lineInfo));
+		}
+		return General.block("crosshair_hor_ranges", compiledLines);
+	}
+}
+
+
+/** Shell distance block (originall named as `crosshair_distances`) */
+export class ShellDistancesBlock {
+	constructor() {
+		this.distanceLines = []
+	}
+
+	/**
+	 * Adds a new shell distance line
+	 *
+	 * @param {number} distance shell distance line value
+	 * @param {number} shown displayed number value, `0` to hide the number
+	 * @param {[number, number]} shownPos position of displayed number
+	 */
+	add(distance, shown = 0, shownPos = [0 ,0]) {
+		this.distanceLines.push({distance, shown, shownPos})
+	}
+
+	getCode() {
+		this.distanceLines.sort((a, b) => (a.distance - b.distance))
+
+		let compiledLines = []
+		for (let distanceInfo of this.distanceLines) {
+			compiledLines.push(General.block("distance", [
+				General.variable("distance", [ distanceInfo.distance, distanceInfo.shown, 0 ]),
+				General.variable("textPos", distanceInfo.shownPos)
+			], {useOneLine: true}))
+		}
+
+		return General.block("crosshair_distances", compiledLines);
+	}
+}
+
+
+export class CirclesBlock {
+	constructor() {
+		this.blockLines = []
+	}
+
+	/**
+	 * Adds a new circle. Only its code will be kept
+	 * @param {Circle} c
+	 */
+	add(c) {
+		this.blockLines.push(c.getCode());
+	}
+
+	/** Adds a text line. Note leading double slash is auto generated by default */
+	addComment(s, prefix = "// ") {
+		this.blockLines.push(prefix + s);
+	}
+
+	getCode() {
+		return General.block("drawCircles", this.blockLines);
+	}
+}
+
+// TODO:
+// lines: "drawLines",
+// quads: "drawQuads",
+// texts: "drawTexts",
+
+
+//// ELEMENTS ////
 
 /** A circle element */
 export class Circle {
 	/**
-	 * @param {object} details detailed info about the shape
-	 * @param {[number, number]} details.segment start/end degree of curve
-	 * @param {[number, number]} details.pos center position
-	 * @param {number} details.diameter circle diameter
-	 * @param {number} details.size line width
-	 * @param {boolean} details.move if move with gun zero changes
-	 * @param {boolean} details.thousandth if use thousandth
+	 * @param {object} obj
+	 * @param {[number, number]} obj.segment start/end degree of curve
+	 * @param {[number, number]} obj.pos center position
+	 * @param {number} obj.diameter circle diameter
+	 * @param {number} obj.size (default `1`) line width
+	 * @param {boolean} obj.move (default `false`) if move with gun zero changes
+	 * @param {boolean} obj.thousandth (default `true`) if use thousandth
 	 */
-	constructor(details) {
-		this.details = details;
+	constructor({
+		segment = [0, 360],
+		pos,
+		diameter,
+		size = 1,
+		move = false,
+		thousandth = true
+	} = {}) {
+		this.details = {
+			segment,
+			pos,
+			diameter,
+			size,
+			move,
+			thousandth
+		};
 	}
-
 
 	copy() {
 		return (new Circle(
 			JSON.parse(JSON.stringify(this.details))
 		));
 	}
-
 
 	mirrorSegmentY() {
 		let newSegment = [ (360-this.details.segment[1]), (360-this.details.segment[0]) ]
@@ -167,28 +323,15 @@ export class Circle {
 		return this;
 	}
 
-
 	/**
 	 * Returns the text for drawing a circle
 	 */
 	getCode() {
 		let subVars = [];
 		for (let k in this.details) {
-			subVars.push(General.variable(k, this.details[k], null, "; "));
+			subVars.push(General.variable(k, this.details[k]));
 		}
-		return General.block("circle", subVars, "", "")
-	}
-
-
-	static templateDetails() {
-		return {
-			segment: [0, 360],
-			pos: [0, 0],
-			diameter: 10,
-			size: 1,
-			move: true,
-			thousandth: true
-		};
+		return General.block("circle", subVars, {useOneLine: true})
 	}
 }
 
@@ -197,6 +340,7 @@ export class Circle {
  * A line with selectable breaking
  *
  * TODO: Add capability for adding breaking dashes
+ * TODO: REPLACE
  */
 export class Line {
 	/**
@@ -257,7 +401,7 @@ export class Line {
 		for (let k in this.details) {
 			subVars.push(General.variable(k, this.details[k], null, "; "));
 		}
-		return General.block("line", subVars, "", "")
+		return General.block("line", subVars, {useOneLine: true})
 	}
 
 	// TODO: get parallel code
--- sight_test.js
+++ sight_test.js
@@ -1,36 +1,40 @@
 import {
 	Sight,
 	General as G,
-	MatchVehicleClass,
+	Toolbox as T,
+	MatchVehicleClassBlock,
+	HorizontalThousandthsBlock,
+	ShellDistancesBlock,
+	CirclesBlock,
 	Circle,
 	Line,
 } from "./sight_lib.js";
 
 
 // Prepare sight file with sight name
-let sf = new Sight("testgen_ussrT64B1984_ussrT80B")
+let s = new Sight("testgen_g_vr_hizoom_lus_sf_g_3AroA")
 
 // Basic settings
-sf.append([
-	G.variable("fontSizeMult", 0.8),
-	G.variable("lineSizeMult", 1.5),
+s.append([
+	G.variable("fontSizeMult", 0.5),
+	G.variable("lineSizeMult", 1.6),
 
 	G.variable("rangefinderTextScale", 0.8),
 	G.variable("rangefinderUseThousandth", false),
 	G.variable("rangefinderProgressBarColor1", [255, 255, 255, 216], "c"),
 	G.variable("rangefinderProgressBarColor2", [0, 0, 0, 216], "c"),
-	G.variable("rangefinderHorizontalOffset", 90),
-	G.variable("rangefinderVerticalOffset", -0.04375),
+	G.variable("rangefinderHorizontalOffset", 125),
+	G.variable("rangefinderVerticalOffset", -0.01725),
 
 	G.variable("detectAllyTextScale", 0.8),
-	G.variable("detectAllyOffset", [90, -0.08]),
+	G.variable("detectAllyOffset", [125, -0.045]),
 
 	G.variable("drawDistanceCorrection", true),
-	G.variable("distanceCorrectionPos", [-0.13, 0.06]),
+	G.variable("distanceCorrectionPos", [-0.17, 0.035]),
 
-	G.variable("crosshairDistHorSizeMain", [-0.01, -0.01]),
-	G.variable("crosshairDistHorSizeAdditional", [0.003, 0.0012]),
-	G.variable("distancePos", [0.15, 0]),
+	G.variable("crosshairDistHorSizeMain", [0, 0]),
+	G.variable("crosshairDistHorSizeAdditional", [0.0070, 0.0030]),
+	G.variable("distancePos", [0.005, 0]),
 
 	G.variable("crosshairHorVertSize", [0.5, 0.3]),
 
@@ -38,83 +42,125 @@ sf.append([
 	G.variable("drawCentralLineHorz", false),
 	G.variable("drawSightMask", true),
 
-	"\n// Color of sight",
+	"",
+	"// Color of sight",
 	G.variable("crosshairColor", [0, 200, 40, 255], "c"),
 	G.variable("crosshairLightColor", [180, 0, 0, 255], "c"),
+	""
 ])
 
 // Vehicle match
-sf.append(MatchVehicleClass.buildBlock([
-	"ussr_t_64_b_1984",
-	"ussr_t_80b"
+s.append(MatchVehicleClassBlock.buildBlock([
+	"exp_tank",
+	"germ_erprobungstrager_3_achs_turm"
 ]))
 
+// Horizontal thousandths
+let horiThous = new HorizontalThousandthsBlock()
+for (let t of T.rangeIE(-32, 32, 8)) { horiThous.add(t, Math.abs(t)); }
+for (let t of T.rangeIE(-28, 28, 8)) { horiThous.add(t); }
+s.append(horiThous.getCode())
+
+// Distance lines
+let shellDistances = new ShellDistancesBlock();
+//   Short ticks
+for (let r of T.rangeIE(400, 4000, 800)) { shellDistances.add(r); }
+//   Long ticks - left text
+for (let r of T.rangeIE(800, 4000, 1600)) { shellDistances.add(r, r/100); }
+//   Long ticks - left text
+for (let r of T.rangeIE(1600, 4000, 1600)) { shellDistances.add(r, r/100, [0.0295, 0]); }
+s.append(shellDistances.getCode())
 
 
-sf.append(
-	G.block(Sight.blockTitle.crosshairHorRanges, [], "", "")
-)
-
-
-
-let circleTextLines = [];
-
-// Vertical Rangefinder
-// 100, 200, 400, 800m
-let vrCurveSegments = [[90, 100], [90, 100], [82, 98], [85, 95]]
-let vrCurveDiameters = (() => {
-	let ds = [];
-	let dNum = 33;
-	for (let c=0; c<4; c++) {
-		ds.push(dNum);
-		dNum /= 2;
-	}
-	return ds;
-})();
-let vrDetails = (() => {
-	let rs = [];
-	for (let i=0; i < vrCurveDiameters.length; i++) {
-		rs.push({
-			segment: vrCurveSegments[i],
-			pos: [0, 0],
-			diameter: vrCurveDiameters[i],
-			size: 1.6,
-			move: false,
-			thousandth: true
-		})
-	}
-	return rs;
-})();
-let vrs = (() => {
-	let rs = [];
-	for (let d of vrDetails) {
-		rs.push((new Circle(d)).getCode())
-		rs.push((new Circle(d)).mirrorSegmentY().getCode())
-	}
-	return rs;
-})();
-sf.append(G.block(Sight.blockTitle.circles, vrs))
-
-
-let hLineXs = [[450, 17.25], [15.75, 9], [7.5, 4.825]]
-let ld = []
-for (let x of hLineXs) {
-	Line.templateSimpleDetails()
-	ld.push({
-		line: [x[0], 0, x[1], 0],
+// Circles
+let circles = new CirclesBlock()
+//   Vertical Rangefinder circles
+let rfAssumeWidth = 3.3
+let vertRfParams = [
+	{range: 100, segment: [90, 100]},
+	{range: 200, segment: [90, 100]},
+	{range: 400, segment: [82, 98]},
+	{range: 800, segment: [85, 95]},
+]
+circles.addComment("Vertical Rangefinder")
+for (let p of vertRfParams) {
+	let c = new Circle({
+		segment: p.segment,
+		pos: [0, 0],
+		diameter: T.calcThousandth(rfAssumeWidth, p.range),
+		size: 1.6,
 		move: false,
-		thousandth: true
+		thousandth: true,
 	})
+	circles.addComment(p.range + "m");
+	circles.add(c);
+	circles.add(c.copy().mirrorSegmentY());
 }
-let ls = (() => {
-	let rs = [];
-	for (let d of ld) {
-		rs.push((new Line(d)).getCode())
-		rs.push((new Line(d)).mirrorY().getCode())
-	}
-	return rs;
-})();
-sf.append(G.block(Sight.blockTitle.lines, vrs))
+circles.addComment("", "");
+
+s.append(circles.getCode());
 
 
-console.log(sf.getCurrentText());
+
+
+// let circleTextLines = [];
+
+// // Vertical Rangefinder
+// // 100, 200, 400, 800m
+// let vrCurveSegments = [[90, 100], [90, 100], [82, 98], [85, 95]]
+// let vrCurveDiameters = (() => {
+// 	let ds = [];
+// 	let dNum = 33;
+// 	for (let c=0; c<4; c++) {
+// 		ds.push(dNum);
+// 		dNum /= 2;
+// 	}
+// 	return ds;
+// })();
+// let vrDetails = (() => {
+// 	let rs = [];
+// 	for (let i=0; i < vrCurveDiameters.length; i++) {
+// 		rs.push({
+// 			segment: vrCurveSegments[i],
+// 			pos: [0, 0],
+// 			diameter: vrCurveDiameters[i],
+// 			size: 1.6,
+// 			move: false,
+// 			thousandth: true
+// 		})
+// 	}
+// 	return rs;
+// })();
+// let vrs = (() => {
+// 	let rs = [];
+// 	for (let d of vrDetails) {
+// 		rs.push((new Circle(d)).getCode())
+// 		rs.push((new Circle(d)).mirrorSegmentY().getCode())
+// 	}
+// 	return rs;
+// })();
+// s.append(G.block(Sight.blockName.circles, vrs))
+
+
+// let hLineXs = [[450, 17.25], [15.75, 9], [7.5, 4.825]]
+// let ld = []
+// for (let x of hLineXs) {
+// 	Line.templateSimpleDetails()
+// 	ld.push({
+// 		line: [x[0], 0, x[1], 0],
+// 		move: false,
+// 		thousandth: true
+// 	})
+// }
+// let ls = (() => {
+// 	let rs = [];
+// 	for (let d of ld) {
+// 		rs.push((new Line(d)).getCode())
+// 		rs.push((new Line(d)).mirrorY().getCode())
+// 	}
+// 	return rs;
+// })();
+// s.append(G.block(Sight.blockName.lines, vrs))
+
+
+console.log(s.getCurrentText());
